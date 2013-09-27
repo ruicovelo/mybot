b6607d9153c42350c9366c810734b2889073be08
Rui Covelo <rui.covelo@gmail.com>
2013-09-27 11:31:08 +0100

    Initial commit

---
+++ MyBot/commandtranslate.py
@@ -0,0 +1,25 @@
+
+
+class BotCommandTranslator(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self,params):
+        '''
+        Constructor
+        '''
+        
+
+
+
+def main():
+    # demo code
+    translator = BotCommandTranslator()
+    while True:
+        line = raw_input()
+
+
+if __name__ == '__main__':
+    main()
