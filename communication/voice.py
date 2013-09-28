a89f90efc54f6430bf3b058839f664bf2dfd88f9
Rui Covelo <rui.covelo@gmail.com>
2013-09-28 17:52:27 +0100

    Adding back voice.py

---
+++ communication/voice.py
@@ -0,0 +1,37 @@
+# Nothing useful here yet. Just fun.
+# used for calling say or speak
+import subprocess
+import sys
+
+
+class Voice: 
+    
+    _shell_command=None
+    mute = False
+    debug = True
+    
+    
+    
+    def __init__(self,debug=False):
+        self.debug = debug
+        #TODO: check availability of shell commands
+        # if platform is 'darwin' I'm using my conding laptop
+        if sys.platform == 'darwin':
+            self._shell_command='say'
+        else: #TODO: check Linux
+            self._shell_command="speak"
+        
+        
+    
+
+    def speak(self,text):
+        if not self.mute and text:
+            try:
+                if self.debug:
+                    print self._shell_command, text
+                    
+                result = subprocess.call([self._shell_command,text])
+                if result == 0:
+                    return True
+            except:
+                return False
