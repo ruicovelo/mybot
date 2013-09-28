38bd868a031f7bcbdf62b184a6cedd3fb362f6d3
Rui Covelo <rui.covelo@gmail.com>
2013-09-28 18:53:12 +0100

    Minor changes

--- console.py
+++ console.py
@@ -12,10 +12,6 @@ from asyncconsole import AsyncConsole
 IN_CON_SOCKET_PATH = 'input_console_socket'
 OUT_CON_SOCKET_PATH = 'output_console_socket'
 
-try:
-    os.unlink(OUT_CON_SOCKET_PATH)
-except:
-    pass
 
 def handle_received_output(s,console):
     ''' To be run by a thread that waits for incoming data and sends it for display in a AsyncConsole object '''
@@ -38,15 +34,19 @@ def main(stdscr):
 
     t = Thread(target=handle_received_output,args=(out_con_socket,console,))
     t.start()
-    
-    while console.readline():
-        if console.input_string == 'quit':
-            break
-        in_con_socket.sendall(console.input_string)
-    in_con_socket.close()
-    out_con_socket.close()                  
+    try:
+        while console.readline():
+            in_con_socket.sendall(console.input_string)
+    finally:
+        in_con_socket.close()
+        out_con_socket.close()
+        os.unlink(OUT_CON_SOCKET_PATH)                  
 
 
 
 if __name__ == '__main__':
+    try:
+        os.unlink(OUT_CON_SOCKET_PATH)
+    except:
+        pass
     curses.wrapper(main)
