656e6bcceccc0082786930a671faabf0d264cf3d
Rui Covelo <rui.covelo@gmail.com>
2013-10-09 08:55:41 +0100

    Now useing mythreading ReceiveSocketThread. Preparing for better client side termination

--- console.py
+++ console.py
@@ -1,48 +1,36 @@
 import curses
-from threading import Thread
 import socket
 import os
-
-
+from mythreading import ReceiveSocketThread
 from asyncconsole import AsyncConsole
-
-
-
 
 IN_CON_SOCKET_PATH = 'in_console_socket'
 OUT_CON_SOCKET_PATH = 'out_console_socket'
 
-
-def handle_received_output(s,console):
-    ''' To be run by a thread that waits for incoming data and sends it for display in a AsyncConsole object '''
-    conn,addr = s.accept()
-    while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        if console:
-            console.addline(data)
-    
+def handle_received_output(data):
+    global console
+    console.addline(data)
 
 def main(stdscr):
+    global console
     console = AsyncConsole(stdscr)
     in_con_socket = socket.socket(socket.AF_UNIX,socket.SOCK_STREAM)
     in_con_socket.connect(IN_CON_SOCKET_PATH)
     out_con_socket = socket.socket(socket.AF_UNIX,socket.SOCK_STREAM)
     out_con_socket.bind(OUT_CON_SOCKET_PATH)
     out_con_socket.listen(1)
-
-    t = Thread(target=handle_received_output,args=(out_con_socket,console,))
+    conn,addr = out_con_socket.accept()
+    t = ReceiveSocketThread(processing_function=handle_received_output,connection=conn)
     t.start()
     try:
         while console.readline():
             in_con_socket.sendall(console.input_string)
+    except KeyboardInterrupt:
+        pass
     finally:
         in_con_socket.close()
         out_con_socket.close()
         os.unlink(OUT_CON_SOCKET_PATH)                  
-
-
 
 if __name__ == '__main__':
     try:
