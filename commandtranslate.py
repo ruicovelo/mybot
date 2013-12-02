e6b90591c5dad3e669d2f72a94acc4b3527728ab
ruicovelo <rui.covelo@gmail.com>
2013-12-02 22:14:27 +0000

    Enhanced conversation features. Minor fix when using only non-alphanumeric values for command line

--- commandtranslate.py
+++ commandtranslate.py
@@ -33,10 +33,10 @@ class BotCommandTranslator(object):
         self._common_commands=common_commands
         self._conversation_timeout_secs = conversation_timeout_secs
 
-    def add_commands(self,module_name,commands):
-        if not self._commands.has_key(module_name):
-            self._commands[module_name]=[]    
-        self._commands[module_name].extend(commands)
+    def add_commands(self,destination_name,commands):
+        if not self._commands.has_key(destination_name):
+            self._commands[destination_name]=[]    
+        self._commands[destination_name].extend(commands)
         
     def remove_destination(self,destination_name):
         try:
@@ -47,26 +47,53 @@ class BotCommandTranslator(object):
         except ValueError:
             pass
     
+    def end_conversation(self):
+        self._last_current_destination_change = None
+        self._current_destination = None
+        
+    def start_conversation(self,destination):
+        if self._current_destination:
+            self.end_conversation()
+        self._set_current_destination(destination)
+        
     def _set_current_destination(self,destination):
         self._current_destination = destination
         self._last_current_destination_change = time()
         
-    def _get_current_destination(self):
-        if self._last_current_destination_change:
+    def _get_current_destination(self,update=True):
+        '''
+        Checks if current_destination is up to date and returns it.
+        '''
+        if update and self._last_current_destination_change:
             if (time() - self._last_current_destination_change) > self._conversation_timeout_secs :
-                self._last_current_destination_change = None
-                self._current_destination = None
+                self.end_conversation()
             else:
                 self._last_current_destination_change = time()
         return self._current_destination
+    
+    def get_current_destination(self,update=False):
+        '''
+        Gets current_destination name but does not check if it's up to date
+        '''
+        destination = self._get_current_destination(update)
+        if destination:
+            return destination
+        else:
+            return ''
     
     def validate(self,line,origin=None):
         destination = self._get_current_destination()
         
         # split into words 
-        #TODO: test with special characters
         words = re.findall(r"[\w]+",line)
-        
+        if len(words) == 0:
+            # could not translate into words
+            # only non alphanumeric characters?
+            if self._common_commands.__contains__(line) or self._commands[destination].__contains__(line):
+                command = line
+                return BotCommand(destination,command,line,origin)
+            return False
+            
         # check if first word is a destination 
         w = 0
         if self._commands.keys().__contains__(words[w]):
@@ -74,7 +101,7 @@ class BotCommandTranslator(object):
             if len(words) > 1:
                 w = w + 1
             else:
-                self._set_current_destination(destination)
+                self.start_conversation(destination)
                 return True
             
         # check if w word is a command accepted by the destination
@@ -87,7 +114,7 @@ class BotCommandTranslator(object):
 def main():
     # demo code
     modules=[None,'sleeper','console','money']
-    common_commands=['stop','start','status']
+    common_commands=['stop','start','status','ok','bye','.','?']
     module_specific_commands={}
 
     for module in modules:
@@ -103,9 +130,19 @@ def main():
         if s=='':break
         cmd = translator.validate(s)
         if cmd == True:
+            print('Now talking to %s ' % translator.get_current_destination())
             continue
         if cmd:
+            if cmd.command in ['ok','bye','.']:
+                print('%s: Bye!' % translator.get_current_destination())
+                translator.end_conversation()
+                continue
+            if cmd.command in ['?']:
+                print('%s: yes...' % translator.get_current_destination())
+                continue
             print(cmd.tostring())
+        else:
+            print('%s: not sure what that means...' % translator.get_current_destination())
 
         
 if __name__ == '__main__':
